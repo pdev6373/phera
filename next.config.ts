b0970b9a8e60f09bee034ff2d311d644be372b4a
pdev6373 <pdev6373@gmail.com>
2025-08-20 17:51:07 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['randomuser.me', 'ui-avatars.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
