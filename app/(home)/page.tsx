2990937994a2fdba87b3269819a8559e47355581
pdev6373 <pdev6373@gmail.com>
2025-08-20 17:49:23 +0100

    first commit

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useContext } from 'react';
+import { Suspense, useContext } from 'react';
 import { USERS } from '@/api/users';
 import Card from '@/components/Card';
 import { textToSlug } from './layout';
@@ -16,21 +16,24 @@ export default function Home() {
     text.toLowerCase().trim().includes(search.toLowerCase().trim());
 
   return (
-    <div className="grid justify-center gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {USERS.filter((user) => filter(user.firstName) || filter(user.lastName))
-        .filter(
-          (user) => category == 'all' || textToSlug(user.category) == category,
-        )
-        ?.map((user) => (
-          <Card
-            key={user?.id}
-            text={user?.firstName}
-            route={`/users/${user?.id}`}
-            image={user?.profilePictureUrl}
-            bottomData={`Goal: ${user.goals}`}
-            imageWrapperClassName="!aspect-[1.608281727/1]"
-          />
-        ))}
-    </div>
+    <Suspense fallback={<></>}>
+      <div className="grid justify-center gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {USERS.filter((user) => filter(user.firstName) || filter(user.lastName))
+          .filter(
+            (user) =>
+              category == 'all' || textToSlug(user.category) == category,
+          )
+          ?.map((user) => (
+            <Card
+              key={user?.id}
+              text={user?.firstName}
+              route={`/users/${user?.id}`}
+              image={user?.profilePictureUrl}
+              bottomData={`Goal: ${user.goals}`}
+              imageWrapperClassName="!aspect-[1.608281727/1]"
+            />
+          ))}
+      </div>
+    </Suspense>
   );
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,7 +3,7 @@ import Card from '@/components/Card';
 import { USERS } from '@/api/users';
 import { useSearchParams } from 'next/navigation';
 import { textToSlug } from '../(home)/layout';
-import { useContext } from 'react';
+import { Suspense, useContext } from 'react';
 import { MainContext } from '@/context/MainContext';
 
 export default function Users() {
@@ -16,21 +16,24 @@ export default function Users() {
     text.toLowerCase().trim().includes(search.toLowerCase().trim());
 
   return (
-    <div className="grid gap-5 [grid-template-columns:repeat(auto-fit,minmax(min(20em,100%),1fr))] md:gap-6 [@media(min-width:1500px)]:[grid-template-columns:repeat(auto-fit,minmax(min(22em,100%),1fr))]">
-      {USERS.filter((user) => filter(user.firstName) || filter(user.lastName))
-        .filter(
-          (user) => category == 'all' || textToSlug(user.category) == category,
-        )
-        ?.map((user) => (
-          <Card
-            key={user?.id}
-            text={user?.firstName}
-            route={`/users/${user?.id}`}
-            image={user?.profilePictureUrl}
-            bottomData={`Goal: ${user.goals}`}
-            imageWrapperClassName="!aspect-[1.608281727/1]"
-          />
-        ))}
-    </div>
+    <Suspense fallback={<></>}>
+      <div className="grid gap-5 [grid-template-columns:repeat(auto-fit,minmax(min(20em,100%),1fr))] md:gap-6 [@media(min-width:1500px)]:[grid-template-columns:repeat(auto-fit,minmax(min(22em,100%),1fr))]">
+        {USERS.filter((user) => filter(user.firstName) || filter(user.lastName))
+          .filter(
+            (user) =>
+              category == 'all' || textToSlug(user.category) == category,
+          )
+          ?.map((user) => (
+            <Card
+              key={user?.id}
+              text={user?.firstName}
+              route={`/users/${user?.id}`}
+              image={user?.profilePictureUrl}
+              bottomData={`Goal: ${user.goals}`}
+              imageWrapperClassName="!aspect-[1.608281727/1]"
+            />
+          ))}
+      </div>
+    </Suspense>
   );
 }
